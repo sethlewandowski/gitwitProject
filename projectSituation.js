ea1cdddd16e7bc30cdd96f6f9c85792f6917198b
Seth Lewandowski <sethlew@protonmail.com>
2021-04-11 12:03:53 -0700

    one file

--- projectSituation.js
+++ projectSituation.js
@@ -1,21 +1,44 @@
-let clientSuccess = Boolean
-
 const client = {
   problem: {
-    description: 'Get parents to start saving $500 every year for college.',
-    isCorrect: false,
+    description: null,
+    accurate: null,
+    goal: 'Get parents to start saving $500 every year for college.',
   },
 }
 
-const gitwitHired = () => {
-  let clientAssessment = "Client doesn't understand their problem."
-  let actualClientProblem = 'Kids graduate with no plan for their career.'
-  if (clientAssessment && actualClientProblem) {
-    client.problem.description =
-      'Help parents guide children into great careers.'
-    client.problem.isCorrect = true
-    return JSON.stringify(client.problem.description)
-  }
+gitwitHired(client)
+
+function gitwitHired(client) {
+  clientProblemAssessment(client)
+  client.problem.goal = 'Help parents guide children into great careers.'
+  console.log(gitwitBusinessOutcome(client))
 }
 
-console.log(gitwitHired())
+function clientProblemAssessment(client) {
+  let clientProblemAccurate = false
+
+  if (!clientProblemAccurate) {
+    client.problem.description = 'Kids graduate with no plan for their career.'
+    client.problem.accurate = true
+  }
+  return
+}
+
+function gitwitBusinessOutcome(client) {
+  let clientProblemAccurate = client.problem.accurate
+  let clientSuccess
+
+  if (!clientProblemAccurate) {
+    let poorGoals = true
+    if (poorGoals) {
+      let poorSolutions = true
+      if (poorSolutions) {
+        clientSuccess = false
+      }
+    }
+  } else if (clientProblemAccurate) {
+    clientSuccess = true
+  }
+  let gitwitSuccess = clientSuccess
+  return gitwitSuccess
+}
